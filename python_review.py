cb682e74c5c148749ba6c2497a9d068f1a9e4a7f
BhavanaRokati <145142769+BhavanaRokati@users.noreply.github.com>
2025-02-16 17:46:18 +0530

    Update python_review.py

--- python_review.py
+++ python_review.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1Rx7fTUqQU4F4Sby2ZzZrT2cBlsxzL5ef
 """
 
-!pip install google-generativeai
-!pip install streamlit
 
 import streamlit as st
 import google.generativeai as genai
